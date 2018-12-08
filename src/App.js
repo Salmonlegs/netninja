c1d628e79b50c08b40a0fda58d67ee895efe6ecf
salmonlegs <s.hardy11@live.com>
2018-12-08 12:30:58 +0800

    up to tutorial #22

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state = {
@@ -9,12 +11,31 @@ class App extends Component {
       {name: 'Goro', age: 142, belt: 'N/A', id: 3}
     ]
   }
+
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas,
+    })
+  }
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    })
+    this.setState({
+      ninjas: ninjas,
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
         <p>Welcome</p>
-        <Ninjas ninjas={this.state.ninjas}/>
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja}/>
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
